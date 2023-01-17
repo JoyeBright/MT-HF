7682c5f7c560800587ef82ef5dca7fed5db2f07a
Javad Pourmostafa <javad.pourmostafa@gmail.com>
2023-01-17 17:44:35 +0100

    Requirements

--- index.py
+++ index.py
@@ -1,1 +1,24 @@
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+from transformers import (set_seed,
+                          Seq2SeqTrainingArguments, 
+                          EarlyStoppingCallback,
+                          Trainer,
+                          Seq2SeqTrainer,
+                          logging,
+                          DataCollatorForSeq2Seq,
+                          MBartForConditionalGeneration,
+                          MBart50TokenizerFast
+                         )
+import torch
+import yaml
+import os
+from munch import Munch
+import mlflow
+import pandas as pd
+import csv
+from datasets import load_dataset
+import numpy as np
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+from typing import Dict
+import matplotlib.pyplot as plt
+import evaluate
