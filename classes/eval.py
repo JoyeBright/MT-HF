3a61a6b895f12030e0b321146b12c3b85926ec79
Javad Pourmostafa <javad.pourmostafa@gmail.com>
2023-01-22 08:40:59 +0100

    Add some comments to eval

--- classes/eval.py
+++ classes/eval.py
@@ -4,6 +4,7 @@ from classes.main import Main
 
 class Eval(Main):
     def __init__(self):
+        # Inheritance
         super().__init__()
 
     def postprocess_text(self, preds, labels):
@@ -13,23 +14,20 @@ class Eval(Main):
         return preds, labels
 
     def compute_metric(self, eval_preds):
-
+        # Take the model's predictions and labels
         preds, labels = eval_preds
-        print(len(preds))
         preds = preds[0] if isinstance(preds, tuple) else preds
-        print(len(preds))
+        # Decode the predictions
         pred_str =self.tokenizer.batch_decode(preds, skip_special_tokens=True)
-        print(pred_str)
-        
+        # Only take the labels that are not -100
         labels = np.where(labels != -100, labels, self.tokenizer.pad_token_id)
+        # Decode the labels
         label_str = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
-        print(label_str)
-        
         # Some simple post-processing
         pred_str, label_str = self.postprocess_text(pred_str, label_str)
-        
+        # Load sacrebleu
         sacrebleu = evaluate.load("sacrebleu")
-
+        # Compute bleu
         result = sacrebleu.compute(predictions=pred_str, references=label_str)
         print(list(result.keys()))
         result = {"bleu": result["score"]}
